e4439a511bd0c267b1c09578611914ffa24480fd
Fadhail <mochammadfadhail2@gmail.com>
2025-07-26 21:52:28 +0700

    edit cors

--- config/cors.go
+++ config/cors.go
@@ -3,8 +3,7 @@ package config
 var allowedOrigins = []string{
 	"http://localhost:3000/",
 	"https://Fadhail.github.io",
-	"http://localhost:5173/",
-	"http://127.0.0.1:8088/",
+	"https://petshop.xeroon.xyz",
 }
 
 func GetAllowedOrigins() []string {
